6c33e76ea5778d3f7869ed1b4c8d6cac3b469bd2
Sanjeev Kumar <Sanjeev.Kumar@intimetec.com>
2020-03-24 14:07:18 +0530

    changes

---
+++ src/main/java/com/github/bigyan123/T1/App.java
@@ -0,0 +1,13 @@
+package com.github.bigyan123.T1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( new Calc().getInt(2, 8));
+    }
+}
---
+++ src/main/java/com/github/bigyan123/T1/Calc.java
@@ -0,0 +1,13 @@
+package com.github.bigyan123.T1;
+
+public class Calc {
+	
+	public int getInt(int x, int y)
+	{
+		return x+y;
+	}
+	public String getString(String x, String y)
+	{
+		return x+y;
+	}
+}
